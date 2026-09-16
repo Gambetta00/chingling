f58418c8d58be06e40202a4c4cfc22d52e7a64e6
ike2009 <henriquezambiasi2@gmail.com>
2026-09-15 21:21:00 -0300

    começo html perfil

---
+++ perfil.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+
+.topo{
+
+background-color: aqua;
+
+   position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 60px;
+
+        display: flex;
+        align-items: center;
+}
+
+
+
+ h1 { 
+    
+ margin-left: 20px;
+
+height: 300px;
+        display: flex;
+        align-items: center;
+
+    }
+
+    h2{
+
+            position: absolute;
+        left: 50%;
+        transform: translateX(-50%);
+
+    }
+
+    .foto_de_perfil{
+
+ margin-left: 30px;
+
+     position: absolute;
+        left: 100%;
+        transform: translateX(-50%);
+
+        width: 150px;
+    height: 100px;
+    }
+
+    </style>
+
+</head>
+<body>
+    <div class="topo">
+
+<h1>Seu perfil</h1>
+
+
+<h2>Chingling Shop</h2>
+
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUsY32NgzN46CXhGcXkBOwQ52EUkpAiqigEbxRs4wz_21sOKkrBq6gTsY&s=10" alt="
+" class="foto_de_perfil">
+
+    </div>
+</body>
+</html>
